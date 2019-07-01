bff032ef4e605cad06bbf2d8c9a4c37dd7a1a5f1
Javier Mercedes <manuelmercedez10@gmail.com>
2019-07-01 16:38:06 -0400

    Added template css and js files

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Auth::routes([
+    'register' => false
+]);
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
