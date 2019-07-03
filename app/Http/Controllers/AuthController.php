9a75050921845c0954c12d8e8d044e44222f88c0
Javier Mercedes <manuelmercedez10@gmail.com>
2019-07-03 19:41:39 -0400

    Modified authcontroller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,14 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginValidation;
+use App\Repositories\Contracts\AuthRepository;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
     protected $auth;
 
-    public function __construct()
+    public function __construct(AuthRepository $authRepository)
     {
+        $this->auth = $authRepository;
+    }
 
+    /**
+     * @param LoginValidation $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(LoginValidation $request)
+    {
+        $response = $this->auth->login($request);
+        return $response ? response()->json([
+            'access_token' => $response['access_token'],
+            'token_type' => 'Bearer',
+            'expires_at' => $response['expires_at']
+        ]) : response()->json(array(
+            'message' => 'Usuario o contraseña incorrectos.'), 401);
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout(Request $request)
+    {
+        $response = $this->auth->logout($request);
+        return $response ? response()->json(['message' => 'Te has desconectado correctamente.']) : response()->json(['message' => 'Ha ocurrido un error.']);
     }
 }
