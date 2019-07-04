e9cec1266111e06495ee645ca30400cecf97d622
Javier Mercedes <manuelmercedez10@gmail.com>
2019-07-04 10:13:10 -0400

    Created favorite movies models and relations

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -16,13 +16,27 @@ class Movie extends Model implements HasMedia
 
     protected $appends = ['imdb_url', 'image_url'];
 
+    /**
+     * function to get the imdb_url of the movie
+     */
     public function getImdbUrlAttribute()
     {
         return "https://www.imdb.com/title/{$this->imdb_id}/";
     }
 
+    /**
+     * function to get the image for the movie
+     */
     public function getImageUrlAttribute()
     {
         return $this->getMedia('movies')[0]->getFullUrl();
+    }
+
+    /**
+     * Get the users that like this movie
+     */
+    public function users()
+    {
+        return $this->belongsToMany('App\Models\User', 'movieuser');
     }
 }
