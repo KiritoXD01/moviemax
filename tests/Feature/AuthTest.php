587562c869f4b5e2b267e4d7dc13645f4979eeeb
Javier Mercedes <manuelmercedez10@gmail.com>
2019-07-03 20:44:55 -0400

    Modified AuthTest

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -2,11 +2,35 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class AuthTest extends TestCase
 {
+    use DatabaseMigrations, DatabaseTransactions;
 
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->seed('PassportSeeder');
+    }
+
+    /**
+     * @test
+     * test to verify if user can login with the auth api
+     */
+    public function userCanLogin()
+    {
+        $data = [
+            'email' => 'testing@admin.com',
+            'password' => 'password'
+        ];
+
+        $this->post(route('auth.login'), $data)
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'access_token', 'token_type'
+            ]);
+    }
 }
